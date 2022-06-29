38c5becfa2de0f3af141b257603acc42461415cd
TiagoMLucio <tiagolucio100@gmail.com>
2022-06-28 22:07:54 -0300

    :rocket: finish: MDC

--- mdc.asm
+++ mdc.asm
@@ -5,12 +5,12 @@ while:
     SUB X4, X1, X2;
     CBZ X4, end; @ A == B
     AND X5, X4, X3; @ X5 = sign(A - B)
-    CBZ X5, else; @ A < B
+    CBZ X5, else; @ A > B
 if:
-    SUB X1, X1, X2; @ A = A - B
+    SUB X2, X2, X1; @ B = B - A
     B while;
 else: 
-    SUB X2, X2, X1; @ B = B - A
+    SUB X1, X1, X2; @ A = A - B
     B while;
 end:
     STUR X1, [X31, #0];
